d7a4b61b8965483608bc69df7a6f93a670c8c9ab
Drenghel <drenghelgy@gmail.com>
2018-10-06 19:53:10 +0200

    better humans sprites, added people, ysorting, cleanup Character.gd

--- Character.gd
+++ Character.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	is_physics_processing();
 
+func _process(delta):
+	handle_inputs()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	handle_movement()
+	
+func handle_movement():
 	var direction : Vector2 = Vector2(0,0)
 	if Input.is_action_pressed("ui_right"):
 		direction.x += speed
@@ -22,3 +25,7 @@ func _physics_process(delta):
 		direction.y += speed
 	
 	self.move_and_slide(direction.clamped(speed))
+	
+func handle_inputs():
+	if Input.is_action_just_pressed("game_action"):
+		print("action !")
