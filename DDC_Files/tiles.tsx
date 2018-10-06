87f65922375e89e13ac705122be12477c94db13a
Drenghel <drenghelgy@gmail.com>
2018-10-06 15:15:46 +0200

    added testing tiles

---
+++ DDC_Files/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tiles" tilewidth="64" tileheight="64" tilecount="25" columns="5">
+ <image source="tiles.png" width="320" height="320"/>
+</tileset>
