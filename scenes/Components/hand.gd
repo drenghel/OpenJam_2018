aebddb17fad40543a8054f9029987abf6640e796
Drenghel <drenghelgy@gmail.com>
2018-10-07 22:29:04 +0200

    added checking for full hand in add_one_ad_in_hand

--- scenes/Components/hand.gd
+++ scenes/Components/hand.gd
@@ -4,6 +4,8 @@ var add_resource : Resource = preload("res://scenes/Components/FilthyAd.tscn")
 var current_stack : Array
 export var max_in_hand : int
 
+func _ready():
+	current_stack = []
 
 func remove_one_ad_from_hand() -> bool:
 	if current_stack.size() != 0:
@@ -16,11 +18,14 @@ func get_advert_instance() -> Sprite:
 	return add_resource.instance()
 	
 func add_one_ad_in_hand():
-	var instance = get_advert_instance()
-	var newPos : Vector2 = Vector2.ZERO
-	if get_child_count() !=  0:
-		newPos = get_child(get_child_count() - 1).get_position()
-		newPos.y = newPos.y - 2
-	instance.set_position(newPos)
-	current_stack.append(instance)
-	add_child(instance)
+	if current_stack.size() < max_in_hand:
+		var instance = get_advert_instance()
+		var newPos : Vector2 = Vector2.ZERO
+		if get_child_count() !=  0:
+			newPos = get_child(get_child_count() - 1).get_position()
+			newPos.y = newPos.y - 2
+		instance.set_position(newPos)
+		current_stack.append(instance)
+		add_child(instance)
+	else:
+		printerr(self, " - " , " has already maxed out")
