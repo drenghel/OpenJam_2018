b1d2d220afa19f24e0c926fc300a4491cefabe70
Drenghel <drenghelgy@gmail.com>
2018-10-07 21:25:31 +0200

    moved code to hand_rechargeable.gd

---
+++ scenes/Components/hand.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var add_resource : Resource = preload("res://scenes/Components/FilthyAd.tscn")
+var current_stack : Array
+export var max_in_hand : int
+
+
+func remove_one_ad_from_hand() -> bool:
+	if current_stack.size() != 0:
+		current_stack.pop_back().queue_free()
+		return true
+	else:
+		return false
+	
+func get_advert_instance() -> Sprite:
+	return add_resource.instance()
+	
+func add_one_ad_in_hand():
+	var instance = get_advert_instance()
+	var newPos : Vector2 = Vector2.ZERO
+	if get_child_count() !=  0:
+		newPos = get_child(get_child_count() - 1).get_position()
+		newPos.y = newPos.y - 2
+	instance.set_position(newPos)
+	current_stack.append(instance)
+	add_child(instance)
---
+++ scenes/Components/hand_rechargeable.gd
@@ -0,0 +1,28 @@
+extends "res://scenes/Components/hand.gd"
+
+onready var giving_range : Area2D = get_node("../giving_range")
+
+func try_to_reload() -> bool:
+	var first_reservoir : Node2D = giving_range.find_first_reservoir_in_list()
+	print("FROM : ", self, " | ", first_reservoir)
+	if first_reservoir:
+#		print("reloading ", max_in_hand - current_stack.size(), " ads")
+		for x in range(current_stack.size(), max_in_hand):
+			add_one_ad_in_hand()
+		return true
+	return false
+	
+
+	
+func try_to_give_to(receiver : KinematicBody2D):
+	if receiver.is_in_group("People") && receiver.has_method("receive_ad"):
+		if receiver.receive_ad():
+			if not remove_one_ad_from_hand():
+				print("No ad to give left !")
+			
+
+func _process(delta):
+	if Input.is_action_just_pressed("game_reload"):
+		if not try_to_reload():
+			print("no reservoir in range")
+
