c569ef62b227bcd56860e5e4ef7b786b9fccaf64
Drenghel <drenghelgy@gmail.com>
2018-10-08 20:39:15 +0200

    Added speeches to People (ugly way) and it's font

--- scenes/People.gd
+++ scenes/People.gd
@@ -2,14 +2,19 @@ extends KinematicBody2D
 
 export var difficulty : float
 
+onready var speech = $speech
+const speech_node_ok : Resource = preload("res://scenes/speech_ok.tscn")
+const speech_node_nok : Resource = preload("res://scenes/speech_nok.tscn")
+
 func receive_ad() -> bool:
 	var roll : int = rand_range(0, 100)
 	if $hand.current_stack.size() < $hand.max_in_hand:
 		if roll > difficulty:
 			$hand.add_one_ad_in_hand()
+			speech.show_sprite_dialog_to_rand_pos(speech_node_ok.instance())
 			return true
 		else:
-			print("Don't want one !")
+			speech.show_sprite_dialog_to_rand_pos(speech_node_nok.instance())
 			return false;
 	else:
 		print("Already got one")
